322b6ad0e5c3d9d7b6a0d02198d1bad591de31dd
Alex Tim <buildmnt@gmail.com>
2023-04-03 16:36:29 +0300

    initial

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,29 @@
+import http from "node:http";
+
+const prefix = '/backend';
+const port = 3000;
+const host = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+  const urlPath = req.url;
+  console.log(urlPath);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  if (urlPath === `${prefix}/health`) {
+    res.end(
+      JSON.stringify({
+      uptime: process.uptime(),
+      timestamp: Date.now(),
+      })
+    );
+  } else {
+    res.end(
+      JSON.stringify({
+        url: urlPath,
+      })
+    );
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Listening for request on ${host}:${port}`);
+});
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from "react";
 import { Button } from "ui";
 
+type ApiData = {
+  uptime?: number;
+  timestamp?: number;
+  url?: string;
+};
+
 export default function Web() {
+  const [data, setData] = useState<ApiData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const res = await fetch('https://nelgroup.biz/backend/health');
+        if (!res.ok) {
+          throw new Error(`Error: ${res.status} ${res.statusText}`);
+        }
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError((err as Error).toString());
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  
   return (
     <div>
       <h1>Web</h1>
+      {data?.uptime && (
+        <div>
+          <span>uptime:</span>
+          <span>data.uptime</span>
+        </div>
+      )}
+      {data?.timestamp && (
+        <div>
+          <span>timestamp:</span>
+          <span>data.timestamp</span>
+        </div>
+      )}
+      {data?.url && (
+        <div>
+          <span>url:</span>
+          <span>data.url</span>
+        </div>      
+      )}
       <Button />
     </div>
   );
