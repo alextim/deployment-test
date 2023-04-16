e37a60aab57e3104a17a724ff6b247a9c0aee5a6
Alex Tim <buildmnt@gmail.com>
2023-04-16 23:10:04 +0300

    fix: cors

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://app.nelgroup.biz" },
+        ],
+      },
+    ];
+  },
 };
